5eca42a469ec08ce387d1183a1999159eaf03733
haizlin <80285586@qq.com>
2017-11-28 10:54:51 +0800

    添加favicon图标

--- docs/template/js/site.js
+++ docs/template/js/site.js
@@ -1,4 +1,5 @@
-$('#search-text').click(function(){
+// 搜索框弹窗
+$('#search-text').click(function() {
     layer.open({
         type: 1,
         title: '搜索',
@@ -8,4 +9,30 @@ $('#search-text').click(function(){
         area: ['650px', '486px'],
         content: '这是搜索框的内容'
     });
+});
+
+// 如果正文高度不够高时，底部固定在最底
+$(function(){
+    var resizeTimer;
+
+    function footerHeight() {
+        var winHeight = $(window).height();
+        var rightHeight = $('.main-right').outerHeight(true);
+        var articleHeight = $('.container article').height();
+        var footerHeight = $('.main-right footer').outerHeight(true);
+        console.log(winHeight, rightHeight, articleHeight, footerHeight);
+
+        if (rightHeight <= winHeight) {
+            var _height = winHeight - rightHeight + articleHeight + 'px';
+            
+            $('.container article').height(_height);
+        }
+    };
+
+    $(window).resize(function() {
+        clearTimeout(resizeTimer);
+        resizeTimer = setTimeout(footerHeight, 250);
+    });
+
+    footerHeight();
 });
